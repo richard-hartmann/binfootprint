5c28ea5076e9e4749c3d1f0078f62740dfcae82e
Richard Hartmann <richard_hartmann@gmx.de>
2022-12-21 13:51:04 +0100

    renaming bug fix

--- tests/test_shelve_cache.py
+++ tests/test_shelve_cache.py
@@ -29,25 +29,25 @@ def test_shelve_cache():
     r = fnc(p)
     assert r[0] == -8
     assert r[1] == 2
-    assert fnc(p, shelve_cache_flag="has_key") is True
+    assert fnc(p, _cache_flag="has_key") is True
 
-    assert fnc(p, a=2, shelve_cache_flag="has_key") is False
-    fnc(p, a=2, shelve_cache_flag="no_cache")
-    assert fnc(p, a=2, shelve_cache_flag="has_key") is False
+    assert fnc(p, a=2, _cache_flag="has_key") is False
+    fnc(p, a=2, _cache_flag="no_cache")
+    assert fnc(p, a=2, _cache_flag="has_key") is False
     fnc(p, a=2)
-    assert fnc(p, a=2, shelve_cache_flag="has_key") is True
+    assert fnc(p, a=2, _cache_flag="has_key") is True
 
     with shelve.open(fnc.f_name) as db:
         key = fnc.param_hash(p, a=2)
         db[key] = None
 
     assert fnc(p, a=2) is None
-    r = fnc(p, a=2, shelve_cache_flag="update")
+    r = fnc(p, a=2, _cache_flag="update")
     assert r is not None
-    assert fnc(p, a=2, shelve_cache_flag="has_key") is True
+    assert fnc(p, a=2, _cache_flag="has_key") is True
 
     try:
-        fnc(p, a=2, b=0, shelve_cache_flag="cache_only")
+        fnc(p, a=2, b=0, _cache_flag="cache_only")
     except KeyError:
         pass
     else:
